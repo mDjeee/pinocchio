c9ed385c5faec92587f5574aee4825aba3d33b82
islam <isejtniyazov@mail.ru>
2025-05-13 17:56:11 +0500

    fix: base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-  production: false,
+  production: true,
   API_URL: 'https://yourpinocchio.tech:8080',
-  SOCKET_ENDPOINT: 'http://134.209.25.249:8080/'
-  // SOCKET_ENDPOINT: 'ws://192.168.20.94:3040'
-  // API_URL2: 'http://192.168.20.201:32656'
-};
+}
