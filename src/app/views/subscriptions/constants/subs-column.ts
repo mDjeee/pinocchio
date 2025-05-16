cd8b3058b61957ff0b613531074bb5e802c5c88b
islam <isejtniyazov@mail.ru>
2025-05-16 15:54:31 +0500

    feat: add tagble column

--- src/app/views/subscriptions/constants/subs-column.ts
+++ src/app/views/subscriptions/constants/subs-column.ts
@@ -1,3 +1,9 @@
 import { TableColumn } from '../../../shared/interfaces/table-column.interface';
 
-export const subsColumn: TableColumn[] = [];
+export const subsColumn: TableColumn[] = [
+  { header: 'Наименование', field: 'name' },
+  { header: 'Описание', field: 'description' },
+  { header: 'Цена', field: 'price' },
+  { header: 'Период', field: 'periodMonth' },
+  { header: 'Активный', field: 'isActive' },
+];
